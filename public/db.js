c14c3cabda8266f59c853ed8aa1a6cf93ba59ab4
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-05-04 21:43:22 -0700

    added in service worker.js and check database in db.js

--- public/db.js
+++ public/db.js
@@ -1,5 +1,3 @@
-
-// TODO: open  indexedDB
 const indexedDB =
 window.indexedDB ||
 window.mozIndexedDB ||
@@ -11,27 +9,23 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 // TODO: create an object store in the open db
-request.onupgradeneeded = function(event) {
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
-};
-request.onsuccess = function(event) {
-  db = event.target.result;
-
-  // check if app is online before reading from db
-  if (navigator.onLine) {
-    checkDatabase();
-  }
-};
-
-request.onerror = function(event) {
-  console.log(`Woops! ${ event.target.errorCode}`);
-};
-
 // TODO: log any indexedDB errors
 // TODO: add code so that any transactions stored in the db
 // are sent to the backend if/when the user goes online
 // Hint: learn about "navigator.onLine" and the "online" window event.
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+request.onerror = function(event){
+  console.log(event);
+};
+request.onsuccess = function(event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
 
 // TODO: add code to saveRecord so that it accepts a record object for a
 // transaction and saves it in the db. This function is called in index.js
@@ -41,3 +35,30 @@ function saveRecord(record) {
   const store = transaction.objectStore("pending");
   store.add(record);
 }
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readonly");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
